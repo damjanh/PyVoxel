d0b89e591a08f447c9ab50bd160b33f959494e10
damjanh <damjan.hudobreznik@gmail.com>
2023-06-28 23:05:25 +0200

    Procedurally generate world of chunks.

--- world_objects/chunk.py
+++ world_objects/chunk.py
@@ -10,6 +10,7 @@ class Chunk:
         self.m_model = self.get_model_matrix()
         self.voxels: np.array = None
         self.mesh: ChunkMesh = None
+        self.is_empty = True
 
     def get_model_matrix(self):
         m_model = glm.translate(glm.mat4(), glm.vec3(self.position) * CHUNK_SIZE)
@@ -22,16 +23,27 @@ class Chunk:
         self.mesh = ChunkMesh(self)
 
     def render(self):
-        self.set_uniform()
-        self.mesh.render()
+        if not self.is_empty:
+            self.set_uniform()
+            self.mesh.render()
 
     def build_voxels(self):
+        # empty
         voxels = np.zeros(CHUNK_VOLUME, dtype='uint8')
 
+        # fill
+        cx, cy, cz = glm.ivec3(self.position) * CHUNK_SIZE
         for x in range(CHUNK_SIZE):
             for z in range(CHUNK_SIZE):
-                for y in range(CHUNK_SIZE):
-                    voxels[x + CHUNK_SIZE * z + CHUNK_AREA * y] = (
-                        x + y + z if int(glm.simplex(glm.vec3(x, y, z) * 0.1) + 1) else 0
-                    )
+                wx = x + cx
+                wz = z + cz
+                world_height = int(glm.simplex(glm.vec2(wx, wz) * 0.01) * 32 + 32)
+                local_height = min(world_height - cy, CHUNK_SIZE)
+
+                for y in range(local_height):
+                    wy = y + cy
+                    voxels[x + CHUNK_SIZE * z + CHUNK_AREA * y] = wy + 1
+
+        if np.any(voxels):
+            self.is_empty = False
         return voxels
